261edc8c62f43b0bc9dd8c37a144d04ce816dc15
ItsTheSanchari <sanchari678@gmail.com>
2021-04-25 17:34:57 +0530

    worked on async queue

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 const async = require('async');
 const _ = require('lodash');
-
-//async queue
+//*****************async queue****************
 const tasksList = _.times(10, _.uniqueId.bind(null, 'task_'));
-const tasksQueue = async.queue((task,cb) => {
-    console.log('task'+task.name)
-    setTimeout(() =>{
-        cb()
-    },3000 )
-},3)
-//after all process gets processed
-tasksQueue.drain(()=> {
-    console.log('all elements processed')
+const tasksQueue = async.queue((task, cb) => {
+    console.log('task' + task.name)
+    setTimeout(() => { cb() }, 3000)
+}, 3)
+//after all process gets processed
+tasksQueue.drain(() => {
+    console.log('all elements processed')
 })
-_.each(tasksList,(eachTask) => {
+_.each(tasksList, (eachTask) => {
     tasksQueue.push({
-        name:eachTask
+        name: eachTask
     })
 })
-//if you have any prioritize element you can push that on top of everything
-tasksQueue.unshift({
-    name:'task_0'
-})
+//if you have any prioritize element you can push that on top of everything
+tasksQueue.unshift({    name:'task_0'})
